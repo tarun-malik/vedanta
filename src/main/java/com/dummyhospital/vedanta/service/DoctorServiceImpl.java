5d1f2dbb1bfe0ae04b00e136b10699d132344c1f
tarun-malik <tarunmalik909@gmail.com>
2025-08-28 23:24:47 +0530

    Implement all methods of DoctorService

--- src/main/java/com/dummyhospital/vedanta/service/DoctorServiceImpl.java
+++ src/main/java/com/dummyhospital/vedanta/service/DoctorServiceImpl.java
@@ -8,6 +8,7 @@ import com.dummyhospital.vedanta.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -56,16 +57,70 @@ public class DoctorServiceImpl implements DoctorService {
 
     @Override
     public List<DoctorResponseDto> getAllDoctors() {
-        return List.of();
+        List<Doctor> doctorList = new ArrayList<>(doctorRepository.findAll());
+
+        List<DoctorResponseDto> doctorResponseDtoList = new ArrayList<>();
+
+        for(Doctor doctor : doctorList) {
+            DoctorResponseDto doctorResponseDto = new DoctorResponseDto();
+
+            doctorResponseDto.setId(doctor.getId());
+            doctorResponseDto.setName(doctor.getName());
+            doctorResponseDto.setCity(doctor.getCity());
+            doctorResponseDto.setEmail(doctor.getEmail());
+            doctorResponseDto.setPhone(doctor.getPhone());
+            doctorResponseDto.setSpeciality(doctor.getSpeciality());
+
+            doctorResponseDtoList.add(doctorResponseDto);
+        }
+
+        return doctorResponseDtoList;
     }
 
     @Override
     public DoctorResponseDto updateDoctor(Long id, DoctorRequestDto doctorRequestDto) {
-        return null;
+        Doctor doctor = doctorRepository.findById(id).orElse(null);
+
+        if(doctor != null) {
+            doctor.setName(doctorRequestDto.getName());
+            doctor.setCity(doctorRequestDto.getCity());
+            doctor.setEmail(doctorRequestDto.getEmail());
+            doctor.setPhone(doctorRequestDto.getPhone());
+            doctor.setSpeciality(doctorRequestDto.getSpeciality());
+
+            doctorRepository.save(doctor);
+        }
+
+        DoctorResponseDto doctorResponseDto = new DoctorResponseDto();
+        doctorResponseDto.setId(doctor.getId());
+        doctorResponseDto.setName(doctor.getName());
+        doctorResponseDto.setCity(doctor.getCity());
+        doctorResponseDto.setEmail(doctor.getEmail());
+        doctorResponseDto.setPhone(doctor.getPhone());
+        doctorResponseDto.setSpeciality(doctor.getSpeciality());
+
+        return doctorResponseDto;
     }
 
     @Override
     public GenericResponseDto removeDoctor(Long id) {
-        return null;
+        Doctor doctor = doctorRepository.findById(id).orElse(null);
+
+        GenericResponseDto genericResponseDto = new GenericResponseDto();
+
+        if(doctor != null) {
+            String name = doctor.getName();
+            doctorRepository.deleteById(id);
+
+            genericResponseDto.setSuccess(true);
+            genericResponseDto.setMessage("Doctor name: " + name + " has been removed successfully");
+
+            return genericResponseDto;
+        }
+
+        genericResponseDto.setSuccess(false);
+        genericResponseDto.setMessage("Doctor id: " + id + " doesn't exist");
+
+        return genericResponseDto;
     }
 }
