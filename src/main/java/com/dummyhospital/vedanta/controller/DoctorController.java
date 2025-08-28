d3becca5a116561977745ffbd356b8802fee7a3d
tarun-malik <tarunmalik909@gmail.com>
2025-08-28 23:26:12 +0530

    Create API endpoint of each service

--- src/main/java/com/dummyhospital/vedanta/controller/DoctorController.java
+++ src/main/java/com/dummyhospital/vedanta/controller/DoctorController.java
@@ -2,11 +2,14 @@ package com.dummyhospital.vedanta.controller;
 
 import com.dummyhospital.vedanta.dto.request.DoctorRequestDto;
 import com.dummyhospital.vedanta.dto.response.DoctorResponseDto;
+import com.dummyhospital.vedanta.dto.response.GenericResponseDto;
 import com.dummyhospital.vedanta.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/doctor")
@@ -25,5 +28,18 @@ public class DoctorController {
         return new ResponseEntity<>(doctorService.getDoctor(id), HttpStatusCode.valueOf(200));
     }
 
+    @GetMapping
+    public ResponseEntity<List<DoctorResponseDto>> getAllDoctors() {
+        return new ResponseEntity<>(doctorService.getAllDoctors(), HttpStatusCode.valueOf(200));
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<DoctorResponseDto> updateDoctor(@PathVariable Long id, @RequestBody DoctorRequestDto doctorRequestDto) {
+        return new ResponseEntity<>(doctorService.updateDoctor(id, doctorRequestDto), HttpStatusCode.valueOf(200));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<GenericResponseDto> removeDoctor(@RequestParam Long id) {
+        return new ResponseEntity<>(doctorService.removeDoctor(id), HttpStatusCode.valueOf(200));
+    }
 }
